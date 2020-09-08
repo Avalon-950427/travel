33e0ae1d81b815c5b2e5b956b5bb0fb4e8fc80de
Avalon <m17625935728@163.com>
2020-09-08 21:44:31 +0800

    index swiper

---
+++ src/views/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wrapper">
+     <!-- ref="mySwiper" @someSwiperEvent="callback" -->
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(item,index) in swiperList" :key="index">
+        <img class="swiper-img" :src="item" alt="">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+      <!-- <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div>
+      <div class="swiper-scrollbar" slot="scrollbar"></div> -->
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: ['/imgs/swiper1.jpg', '/imgs/swiper2.jpg']
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active{
+    background: white !important;
+  }
+.wrapper{
+  overflow: hidden
+  width: 100%
+  height: 4rem
+  .swiper-img{
+    width: 100%;
+    height: 4rem;
+  }
+}
+</style>
